d88f26b29d68b7769487229b42368487ca588f44
BD <bd002@nym.hush.com>
2018-09-03 22:01:02 -0500

    Fixed minor issues. First working version of JPA based repos

--- src/main/java/bdn/quantum/QuantumApplication.java
+++ src/main/java/bdn/quantum/QuantumApplication.java
@@ -2,8 +2,14 @@ package bdn.quantum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@EntityScan("bdn.quantum.model")
+@EnableJpaRepositories("bdn.quantum.repository")
 public class QuantumApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,24 @@
+
+CREATE TABLE IF NOT EXISTS `quantum`.`transaction` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `sec_id` INT UNSIGNED NOT NULL,
+  `user_id` INT UNSIGNED NOT NULL,
+  `tran_date` DATETIME NOT NULL,
+  `type` VARCHAR(3) NOT NULL,
+  `shares` DOUBLE NOT NULL,
+  `price` DOUBLE NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+CREATE TABLE IF NOT EXISTS `quantum`.`security` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `basket_id` INT UNSIGNED NOT NULL,
+  `symbol` VARCHAR(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
+
+CREATE TABLE IF NOT EXISTS `quantum`.`basket` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC));
