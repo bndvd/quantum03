74ec6070a910a6c1570143354b4cdbade3cd5306
BD <bd002@nym.hush.com>
2018-09-25 00:50:42 -0500

    Built initial version of transactions page

--- src/main/resources/js/quantum.js
+++ src/main/resources/js/quantum.js
@@ -25,18 +25,41 @@ app.config(function($routeProvider) {
 
 // Dashboard Controller
 app.controller('dashboardCtrl', function($scope, $http) {
+	$scope.assetsTotalPrincipal = 0.0;
+	$scope.assetsTotalLastValue = 0.0;
+	$scope.assetsTotalCapGain = 0.0;
+
 	$http.get("api/v1/assets").then(function(response) {
-		$scope.assetsTotalPrincipal = 0.0;
-		$scope.assetsTotalLastValue = 0.0;
 		$scope.assets = response.data;
 		for (i = 0; i < $scope.assets.length; i++) {
 			$scope.assetsTotalPrincipal += $scope.assets[i].principal;
 			$scope.assetsTotalLastValue += $scope.assets[i].lastValue;
 		}
 		$scope.assetsTotalCapGain = $scope.assetsTotalLastValue - $scope.assetsTotalPrincipal;
-	});
-	
-
+	});	
 });
 
+// Transaction Controller
+app.controller('transactionsCtrl', function($scope, $http) {
+	$scope.securitiesSelectedTransactionsIndex = -1;
+	$scope.securitiesSelectedTransactions = [];
+	
+	$http.get("api/v1/securities").then(function(response) {
+		$scope.securities = response.data;
+	});
+	
+	$scope.loadTransactionsForSecurity = function(index) {
+		$scope.securitiesSelectedTransactionsIndex = index;
+		if (index >= 0 && index < $scope.securities.length) {
+			var secId = index + 1;
+			$http.get("api/v1/transactions/"+secId).then(function(response) {
+				$scope.securitiesSelectedTransactions = response.data;
+			});
+		}
+		else {
+			$scope.securitiesSelectedTransactions = [];
+		}
+	};
+	
+});
 
