80edc3b3fe55f62fad8e56c587d61eb98ac0e8a2
BD <bd002@nym.hush.com>
2018-10-13 15:48:39 -0500

    Added more login elements to UI

--- src/main/resources/js/login.js
+++ src/main/resources/js/login.js
@@ -5,6 +5,7 @@ app.controller("navCtrl", function($scope, $http) {
 	
 	$scope.authSuccess = false;
 	$scope.authError = null;
+	$scope.createError = null;
 	
 	$scope.loginMode = $scope.LOGIN_MODE_SIGNIN;
 	$scope.loginUsername = null;
@@ -14,10 +15,33 @@ app.controller("navCtrl", function($scope, $http) {
 	$scope.loginNewPasswordRetype = null;
 	
 	
+	$scope.selectLoginMode = function(newMode) {
+		$scope.authError = null;
+		$scope.createError = null;
+		$scope.loginUsername = null;
+		$scope.loginPassword = null;
+		$scope.loginNewUsername = null;
+		$scope.loginNewPassword = null;
+		$scope.loginNewPasswordRetype = null;
+		
+		$scope.loginMode = newMode;
+	};
+	
 	$scope.login = function() {
 		if ($scope.loginUsername == null || $scope.loginUsername.trim() == "" ||
-				$scope.loginUsername == null || $scope.loginUsername.trim() == "") {
-			// TODO
+				$scope.loginPassword == null || $scope.loginPassword.trim() == "") {
+			$scope.authError = "Username and password must be non-blank";
+			return;
+		}
+		// TODO
+	};
+	
+	$scope.createUsername = function() {
+		if ($scope.loginNewUsername == null || $scope.loginNewUsername.trim() == "" ||
+				$scope.loginNewPassword == null || $scope.loginNewPassword.trim() == "" ||
+				$scope.loginNewPasswordRetype == null || $scope.loginNewPasswordRetype.trim() == "" ||
+				$scope.loginNewPassword != $scope.loginNewPasswordRetype) {
+			$scope.createError = "Username and password must be non-blank";
 			return;
 		}
 		// TODO
